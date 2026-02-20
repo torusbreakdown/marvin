ae9f9c5ce330a9a76522233cdeabfe8fedfbf62e
kmd <kevinmichaeldaley@gmail.com>
2026-02-19 18:05:58 -0800

    Removed files we don't need

--- resume.sh
+++
@@ -1,1 +0,0 @@
-copilot --resume=12a9d933-17de-4ff3-8425-12454053c38e
--- test/smoke.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vitest_1 = require("vitest");
-(0, vitest_1.describe)("marvin node smoke", () => {
-    (0, vitest_1.it)("loads test runner", () => {
-        (0, vitest_1.expect)(1 + 1).toBe(2);
-    });
-});
